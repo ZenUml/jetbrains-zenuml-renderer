8577080f6c38e1fd691bc78f6609a0692d02ce5c
GitHub Action <action@github.com>
2022-05-22 12:31:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mrcoder/jetbrainszenumlrenderer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mrcoder.jetbrainszenumlrenderer.services
+
+import com.github.mrcoder.jetbrainszenumlrenderer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mrcoder/jetbrainszenumlrenderer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrcoder/jetbrainszenumlrenderer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrcoder.jetbrainszenumlrenderer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrcoder.jetbrainszenumlrenderer.MyBundle
 
 class MyProjectService(project: Project) {
 
